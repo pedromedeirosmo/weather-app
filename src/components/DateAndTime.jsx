936f277573ce45c87870e5814bdda3a5816c5bf6
Pedro Medeiros <pedromedeirosmo@gmail.com>
2026-04-26 15:38:13 -0300

    date and time icons added

--- src/components/DateAndTime.jsx
+++ src/components/DateAndTime.jsx
@@ -1,8 +1,16 @@
+import { Calendar, Clock } from "lucide-react";
+
 export default function DateAndTime({ date, time }) {
   return (
     <div className="text-center sm:text-left mt-2 space-y-1">
-      <p className="text-white/70 text-sm mt-1 capitalize">{date}</p>
-      <p className="text-white/70 text-sm mt-1">{time.replace("GMT", "UTC")}</p>
+      <div className="flex gap-1 items-center">
+        <Calendar className="text-white/70 w-5" />
+        <p className="text-white/70 text-sm capitalize">{date}</p>
+      </div>
+      <div className="flex gap-1 items-center">
+        <Clock className="text-white/70 w-5" />
+        <p className="text-white/70 text-sm">{time.replace("GMT", "UTC")}</p>
+      </div>
     </div>
   );
 }
